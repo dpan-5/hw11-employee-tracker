5e4055c27380b71af645a5972a69e6a3fb5811c0
David Pan <dpan5425@gmail.com>
2020-10-05 17:50:25 -0400

    added initial prompt and user choices

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,1 +1,27 @@
 const inquirer = require("inquirer");
+const employeeDB_CRUD = require("./employeeDB_CRUD");
+
+const choices = [
+    "View All Employees",
+    "View All Employees By Department",
+    "View All Employees By Manager",
+    "Add Employee",
+    "Remove Employee",
+    "Update Employee Role",
+    "Update Employee Manager"
+];
+
+const init = () => {
+    inquirer.prompt([
+        {
+            name: "userSelection",
+            type: "list",
+            message: "What would you like to do?",
+            choices: choices
+        }
+    ]).then(({ userSelection }) => {
+        console.log(userSelection);
+    });
+}
+
+init();
