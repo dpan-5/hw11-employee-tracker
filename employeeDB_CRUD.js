a8630637566e4969b618929ad0531526dcd56b47
David Pan <dpan5425@gmail.com>
2020-10-06 02:45:26 -0400

    added viewEmployeesByManager() functionality

--- employeeDB_CRUD.js
+++ employeeDB_CRUD.js
@@ -41,17 +41,46 @@ const viewEmployeesByDepartment = (department) => {
 // Query that returns list of departments
 const getDepartments = () => {
     return new Promise((resolve, reject) => {
-        connection.query("SELECT * FROM department", (err, res) => {
+        connection.query("SELECT * FROM department;", (err, res) => {
             if (err) reject(err);
             resolve(res);
         });
     });
 }
 
+// Query that returns list of managers
+const getManagers = () => {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT CONCAT(b.first_name, " ", b.last_name) AS Name 
+        FROM employee a LEFT JOIN employee b
+        ON a.manager_id = b.id
+        WHERE a.manager_id IS NOT NULL
+        GROUP BY a.manager_id;`, (err, res) => {
+            if (err) reject(err);
+            resolve(res);
+        });
+    });
+}
+
+// Query to view all employees by manager
+const viewEmployeesByManager = (manager) => {
+   return new Promise((resolve, reject) => {
+       connection.query(`SELECT CONCAT(a.first_name," " , a.last_name) AS 'Employee Name', a.role_id, CONCAT(b.first_name, " ", b.last_name) AS 'Manager Name'
+       FROM employee a LEFT JOIN employee b
+       ON a.manager_id = b.id
+       WHERE CONCAT(b.first_name, " ", b.last_name) = ?;`, [manager], (err, res) => {
+           if (err) reject(err);
+           console.table(res);
+           resolve();
+       })
+   }) 
+}
 
 
 module.exports = {
     viewEmployees,
     viewEmployeesByDepartment,
-    getDepartments
+    viewEmployeesByManager,
+    getDepartments,
+    getManagers
 }
--- employeeTracker.js
+++ employeeTracker.js
@@ -11,6 +11,19 @@ const choices = [
     "Update Employee Manager"
 ];
 
+console.log("\x1b[34m",
+    `
+|----------------------------------------------------------------------------|                                                             
+|     _____           _                    _____                             |
+|    |   __|_____ ___| |___ _ _ ___ ___   |     |___ ___ ___ ___ ___ ___     |
+|    |   __|     | . | | . | | | -_| -_|  | | | | .'|   | .'| . | -_|  _|    |
+|    |_____|_|_|_|  _|_|___|_  |___|___|  |_|_|_|__,|_|_|__,|_  |___|_|      |
+|                |_|       |___|                            |___|            |
+|                                                                            |
+|----------------------------------------------------------------------------|
+   `
+);
+
 const init = () => {
     inquirer.prompt([
         {
@@ -28,6 +41,7 @@ const init = () => {
                 await viewEmployeesByDepartment();
                 break;
             case "View All Employees By Manager":
+                await viewEmployeesByManager();
                 break;
             case "Add Employee":
                 break;
@@ -44,7 +58,7 @@ const init = () => {
     });
 }
 
-
+// Grabs a list of current departments and populates choices for user to select
 const viewEmployeesByDepartment = async () => {
     const choices = await employeeDB_CRUD.getDepartments();
     return new Promise((resolve, reject) => {
@@ -55,10 +69,31 @@ const viewEmployeesByDepartment = async () => {
                 message: "Please select a department: ",
                 choices: choices
             }
-        ]).then( ({ department }) => {
-            employeeDB_CRUD.viewEmployeesByDepartment(department);
+        ]).then(async ({ department }) => {
+            await employeeDB_CRUD.viewEmployeesByDepartment(department);
+            resolve();
         });
     });
 }
+
+// Grabs a list of current managers and populates choices for user to select
+const viewEmployeesByManager = async () => {
+    const choices = await employeeDB_CRUD.getManagers();
+    const newChoices = choices.map(manager => manager.Name);
+    return new Promise((resolve, reject) => {
+        inquirer.prompt([
+            {
+                name: "manager",
+                type: "list",
+                message: "Please select a manager: ",
+                choices: newChoices
+            }
+        ]).then(async ({ manager }) => {
+            await employeeDB_CRUD.viewEmployeesByManager(manager);
+            resolve();
+        });
+    });
+}
+
 
 init();
